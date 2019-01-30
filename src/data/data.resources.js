360c5e162e9d90976baf70476ddf4335f859702b
Reemtos <reemtos2012@gmail.com>
2019-01-30 18:53:21 +0100

    Initial commit with entire landing page

---
+++ src/data/data.resources.js
@@ -0,0 +1,145 @@
+const Resources = [
+  {
+    categoryName: 'Articles',
+    links: [
+      {
+        title: 'Clean Code Javascript',
+        url: 'https://github.com/ryanmcdermott/clean-code-javascript#table-of-contents'
+      },
+      {
+        title: 'CSS Weekly',
+        url: 'https://css-weekly.com/'
+      },
+      {
+        title: 'Five Best Design Links, Everyday',
+        url: 'https://sidebar.io/'
+      },
+      {
+        title: 'Dev and Design Articles',
+        url: 'https://tympanus.net/codrops/'
+      },
+      {
+        title: 'CSS3, JS & HTML5 Articles',
+        url: 'http://www.standardista.com/'
+      }
+    ]
+  },
+  {
+    categoryName: 'Dev Tools',
+    links: [
+      {
+        title: 'Can I Use',
+        url: 'https://caniuse.com/'
+      },
+      {
+        title: 'Free Background Videos',
+        url: 'https://coverr.co/'
+      },
+      {
+        title: 'Streamline Icons',
+        url: 'https://www.streamlineicons.com/'
+      }
+    ]
+  },
+  {
+    categoryName: 'CSS Tools',
+    links: [
+      {
+        title: 'Cubic Bezier Generator',
+        url: 'http://cubic-bezier.com/'
+      },
+      {
+        title: 'CSS Clip-Path Maker',
+        url: 'http://bennettfeely.com/clippy/'
+      },
+      {
+        title: 'Easing Functions List',
+        url: 'https://easings.net/'
+      },
+      {
+        title: 'CSS Animation Types',
+        url: 'http://animista.net/'
+      },
+      {
+        title: 'Useful :nth-child Recipes',
+        url: 'https://css-tricks.com/useful-nth-child-recipies/'
+      }
+    ]
+  },
+  {
+    categoryName: 'Design Tools and Resources',
+    links: [
+      {
+        title: 'Printable Sketch Sheet Templates',
+        url: 'https://sketchsheets.com/'
+      },
+      {
+        title: 'Vector Icons',
+        url: 'https://www.flaticon.com/'
+      },
+      {
+        title: 'Flat Colors',
+        url: 'http://www.flatuicolorpicker.com/'
+      },
+      {
+        title: 'Fonts In Use',
+        url: 'https://fontsinuse.com/'
+      },
+      {
+        title: 'Color Palette Generator',
+        url: 'https://mycolor.space/'
+      },
+      {
+        title: 'UI Gradients Generator',
+        url: 'https://uigradients.com/#BlueRaspberry'
+      },
+      {
+        title: 'Color Shades Generator',
+        url: 'http://www.0to255.com/'
+      },
+      {
+        title: 'Sketch App Resources',
+        url: 'https://www.sketchappsources.com/'
+      },
+      {
+        title: 'Illustration Creator',
+        url: 'https://www.humaaans.com/'
+      },
+      {
+        title: 'Free Stock Photos',
+        url: 'https://www.pexels.com/'
+      },
+      {
+        title: 'Official Brand Color Codes',
+        url: 'https://brandcolors.net/'
+      },
+      {
+        title: 'Real content for Designs',
+        url: 'https://www.lists.design/'
+      },
+      {
+        title: 'Free Outline Iconset',
+        url: 'http://linea.io/'
+      },
+      {
+        title: 'Free Photos',
+        url: 'https://unsplash.com/'
+      }
+    ]
+  },
+  {
+    categoryName: 'General Tools',
+    links: [
+      {
+        title: 'Text Case Converter',
+        url: 'https://convertcase.net/'
+      },
+      {
+        title: 'Image Size Compressor',
+        url: 'https://tinypng.com/'
+      }
+    ]
+  }
+]
+
+export default Resources
---
+++ src/pages/Home.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+        <header class="section section__header">
+            <div class="intro-section">
+                <div class="logo-holder">
+                    <i class="far fa-bookmark icon"></i>
+                </div>
+                <div class="introduction">
+                    <h1 class="intro-phrase">Tosin’s Bookmarks</h1>
+                    <!-- <p class="intro-paraphrase">A collection of my favourite and valuable design and development resources as used frequently in my projects.</p> -->
+                    <p class="intro-paraphrase">A collection of my favourite design and development resources; because my browser’s bookmarks bar became a mess.</p>
+                </div>
+            </div>
+            <div class="illustration-section">
+                <img src="../assets/images/checklist.png" alt="" class="illustration" />
+            </div>
+        </header>
+        <section class="section section__search">
+            <div class="search-box">
+                <input type="text" class="search-field" placeholder="Search from 70 awesome resources.." name="keyword">
+                <i class="fas fa-search search-icon"></i>
+            </div>
+        </section>
+        <section class="section section__listing">
+            <div class="category" v-for="(category, index) in MyResources" v-bind:key="index">
+                <h3 class="category__title">{{category.categoryName}}</h3>
+                <div class="category__list">
+                    <a v-bind:href="link.url" target="_blank" v-for="(link, index2) in category.links" v-bind:key="index2" class="category__list--link">
+                        <div class="category__list--item">
+                            <div class="info">
+                                <h5 class="title">{{link.title}}</h5>
+                                <h6 class="link">{{link.url}}</h6>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-external-link-alt icon"></i>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </section>
+        <footer class="section section__footer">
+            <p class="info">Created by <a href="#" class="link" target="_blank">Tosin Orimogunje</a>.</p>
+            <p class="info">Illustration from <a href="#" class="link" target="_blank">Undraw</a>.</p>
+        </footer>
+    </div>
+</template>
+
+<script>
+import Resources from '../data/data.resources'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      MyResources: [...Resources]
+    }
+  }
+}
+</script>
